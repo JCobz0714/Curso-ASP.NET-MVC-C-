5d837f24d7ed6647c8392023761abd31cb0abb77
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-06-02 18:39:29 -0500

    Lectura 42 - Creacion de delegates

---
+++ App03/App03/App03/ClassFuncion.cs
@@ -0,0 +1,15 @@
+﻿namespace App03
+{
+    public class ClassFuncion
+    {
+        public string func1(int a, int b)
+        {
+            return (a + b).ToString();
+        }
+
+        public string func2(int a, int b)
+        {
+            return (a * b).ToString();
+        }
+    }
+}
---
+++ App03/App03/App03/Program.cs
@@ -0,0 +1,8 @@
+﻿using App03;
+
+var objeto = new ClassFuncion();
+
+MiDelegate f = objeto.func1;
+Console.WriteLine($"El resultado es {f(10, 100)}");
+
+public delegate string MiDelegate(int arg1, int arg2);
