3d0f20442fdaad803ef38d8facd4fdf441f6f2ee
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-06-02 19:04:14 -0500

    Lectura 42 - Creacion de delegates (Final)

---
+++ App03/App03/App03/MiClase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App03
+{
+    public class MiClase
+    {
+        public string InstanciaDeMetodo(int a, int b)
+        {
+            return ((a + b) * a).ToString();
+        }
+    }
+}
--- App03/App03/App03/Program.cs
+++ App03/App03/App03/Program.cs
@@ -5,4 +5,11 @@ var objeto = new ClassFuncion();
 MiDelegate f = objeto.func1;
 Console.WriteLine($"El resultado es {f(10, 100)}");
 
+f = objeto.func2;
+Console.WriteLine($"El resultado es {f(56, 78)}");
+
+var objeto2 = new MiClase();
+f = objeto2.InstanciaDeMetodo;
+Console.WriteLine($"El resultado de la formula es {f(89, 100)}");
+
 public delegate string MiDelegate(int arg1, int arg2);
