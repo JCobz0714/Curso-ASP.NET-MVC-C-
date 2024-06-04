41635241368c6aca3aaf78e6cd15052b8e4f0d79
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-06-03 19:39:50 -0500

    Lectura 43 - Delegates anonimos

--- App03/App03/App03/Program.cs
+++ App03/App03/App03/Program.cs
@@ -1,4 +1,5 @@
-﻿using App03;
+﻿/*
+using App03;
 
 var objeto = new ClassFuncion();
 
@@ -13,3 +14,19 @@ f = objeto2.InstanciaDeMetodo;
 Console.WriteLine($"El resultado de la formula es {f(89, 100)}");
 
 public delegate string MiDelegate(int arg1, int arg2);
+*/
+
+//Como es un delegate anonimo, puedo agregarle la logica que yo quiera
+//Primero hay que definir el delegate antes de declararlo
+//Un delegate anonimo es un delegate al cual no se le agrega la logica
+//de un metodo ya existente, sino que tiene su propia logica
+MiDelegado f = delegate (int i, string s)
+{
+    return i + 100;
+};
+
+//Llamando al delegate
+int resultado = f(250, "Es Jacobo Osorio");
+
+//Delegates anonimos
+public delegate int MiDelegado(int i, string s);
