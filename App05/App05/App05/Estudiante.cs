466f0a718214f8b99e0da45fd8ec52dd0014e78c
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-07-01 22:00:26 -0500

    Seccion 56 - Diseño de objetos

---
+++ App05/App05/App05/Estudiante.cs
@@ -0,0 +1,47 @@
+﻿namespace App05
+{
+    public class Estudiante : IComparable
+    {
+        //Propiedades que a futuro van a aceptar nulls
+        public string? Nombre { get; set; }
+
+        public string? Apellido { get; set; }
+
+        public Estudiante(string? nombre, string? apellido)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+        }
+
+        //Heredando de la clase "object", cogemos este metodo ToString()
+        public override string ToString()
+        {
+            //Imprime los valores determinados de cada clase, en este caso,
+            //los valores de la clase "Estudiante.cs"
+            return $"{Nombre} {Apellido}";
+        }
+
+        public int CompareTo(object? obj)
+        {
+            if (obj is null) return 1;
+
+            //Si obj es de tipo estudiante, entonces quiero que lo instancie o que haga una representacion de
+            //este objeto tipo Estudiante
+            //Primero se verifica si el objeto es de tipo Estudiante, si lo es
+            //se instancia un objeto tipo estudiante llamado "miEstudiante" con
+            //los valores que tiene el parametro "obj"
+            if (obj is Estudiante miEstudiante)
+            {
+                //Si se repiten apellidos, hacer la comparacion por el nombre
+                if (miEstudiante.Apellido == this.Apellido)
+                {
+                    return this.Nombre!.CompareTo(miEstudiante.Nombre);
+                }
+                //Si los apellidos no son iguales, comparar y organizar por apellido
+                return this.Apellido!.CompareTo(miEstudiante.Apellido);
+            }
+            //Mostrar una excepcion si el objeto no es de tipo "Estudiante"
+            throw new ArgumentException("No es un objeto de tipo Estudiante", nameof(obj));
+        }
+    }
+}
---
+++ App05/App05/App05/Program.cs
@@ -0,0 +1,26 @@
+﻿using App05;
+
+var estudiantes = new Estudiante[10];
+
+estudiantes[0] = new Estudiante("Vaxi", "Drez");
+estudiantes[1] = new Estudiante("Maria", "Lopez");
+estudiantes[2] = new Estudiante("Nestor", "Arcila");
+estudiantes[3] = new Estudiante("Joaquin", "Camino");
+estudiantes[4] = new Estudiante("Roberto", "Dulanto");
+estudiantes[5] = new Estudiante("Juan", "Garcia");
+estudiantes[6] = new Estudiante("Luisa", "Ramirez");
+estudiantes[7] = new Estudiante("Luis", "Ojeda");
+
+//Para organizar una coleccion de objetos, se puede implementar la interface
+//"IComparable".
+//Para hacer que el array se organice debo de llamar a una instancia del
+//"Array", que es una clase utilitaria que tiene metodos como "Sort".
+//Vamos a organizar cada una de las entradas del arreglo en orden alfabetico.
+Array.Sort(estudiantes);
+
+for (int i = 0; i < estudiantes.Length; i++)
+{
+    //No es necesario llamar al metodo ".ToString()" del objeto "Estudiante" para imprimir el contenido del
+    //objeto, ya que "Console.WriteLine" tiene incorporado esa funcion.
+    Console.WriteLine(estudiantes[i]);
+}
