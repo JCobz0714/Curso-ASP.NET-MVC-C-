760070300dca2de19de91d4473df0f449a000bf7
J. cob <55332149+JacoboOsorio@users.noreply.github.com>
2024-08-17 23:10:26 -0500

    Seccion 17 - Lectura 68 - Covariance y contravariance

---
+++ App07/App07/Program.cs
@@ -0,0 +1,26 @@
+﻿Base x = new Base();
+Base y = new Derived();
+
+x.EjecutarEnBase();
+y.EjecutarEnBase();
+
+Console.WriteLine();
+
+//Llamada ilegal de metodo
+//y.EjecutarEnDerivada();
+
+Derived z = new Derived();
+z.EjecutarEnDerivada();
+z.EjecutarEnBase();
+
+//Clase "Less-derived"
+class Base 
+{
+    public void EjecutarEnBase() => Console.WriteLine($"Ejecutando desde {GetType().Name}");
+}
+
+//Clase "More-derived"
+class Derived : Base 
+{
+    public void EjecutarEnDerivada() => Console.WriteLine($"Ejecutando derivada {GetType().Name}");
+}
